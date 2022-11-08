029d288c73edffd50895719aaa420b62d66a59d3
nadunchanna98 <gancp1218@gmail.com>
2022-11-09 01:10:53 +0530

    first commit

--- LAB09_2019E094/task2/ClientThread.java
+++
@@ -1,9 +0,0 @@
-import java.net.Socket;
-
-public class ClientThread extends Thread {
-    public ClientThread(Socket clientSocket, String clientName) {
-    }
-
-    public void start() {
-    }
-}
